9947f8c1771bb638b08f6eaec85766fe3d9f5cf5
unleashy <unleashy@users.noreply.github.com>
2023-01-22 02:14:40 -0300

    Evaluate negation

--- src/Ji.Tests/EvaluatorTests.fs
+++ src/Ji.Tests/EvaluatorTests.fs
@@ -10,3 +10,10 @@ open Ji.Evaluator
 [<Property>]
 let ``Evaluates integers`` (num: NonNegativeInt) =
     Assert.Equal(ExprInt num.Get |> eval, ValueInt num.Get)
+
+[<Fact>]
+let ``Evaluates negation`` () =
+    Assert.Equal(
+        ExprUnary(op = UnaryOp.Neg, expr = ExprInt 1234) |> eval,
+        ValueInt -1234
+    )
--- src/Ji/Evaluator.fs
+++ src/Ji/Evaluator.fs
@@ -3,7 +3,13 @@ module Ji.Evaluator
 open Ji.Ast
 open Ji.Value
 
-let eval (expr: Expr) : Value =
+let evalUnary (op: UnaryOp.T) (value: Value) : Value =
+    match (op, value) with
+    | (UnaryOp.Neg, ValueInt num) -> ValueInt(-num)
+// | _ -> failwith $"Cannot apply unary operator {op} to value {value}"
+
+let rec eval (expr: Expr) : Value =
     match expr with
     | ExprInt(n) -> ValueInt(n)
-    | _ -> failwith $"Unknown AST {expr}"
+    | ExprUnary(op, expr) -> evalUnary op (eval expr)
+// | _ -> failwith $"Unknown AST {expr}"
