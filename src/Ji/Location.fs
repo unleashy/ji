c5a7481cefb0a0c827b8f1b497ff5e6393593383
unleashy <unleashy@users.noreply.github.com>
2023-02-02 02:52:09 -0300

    Use String.length

--- src/Ji/Location.fs
+++ src/Ji/Location.fs
@@ -36,14 +36,14 @@ module Location =
         // The amount of newlines before the span's text is its line.
         //   "ab\ncd\nef"
         //    1   2   3
-        let line = (before |> String.filter (fun c -> c = '\n')).Length
+        let line = before |> String.filter ((=) '\n') |> String.length
 
         // The column is the offset between the last newline before the span and
         // the start of the span. Newline characters themselves count as part of
         // the same line as the characters before them.
         //   "abcd\nef\nghijkl"
         //    12345 123 123456
-        let column = before[before.LastIndexOf("\n") + 1 ..].Length
+        let column = before[before.LastIndexOf("\n") + 1 ..] |> String.length
 
         // + 1 adjust for 1-indexing
         { Line = line + 1; Column = column + 1 }
